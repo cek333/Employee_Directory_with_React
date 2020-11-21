b8c0fbd332ded1ec7b2b211f292cd4fe75060e4c
Cecil <69444825+cek333@users.noreply.github.com>
2020-11-21 17:54:15 -0500

    added filtering and sorting of list

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(function() {
     setEmployeeList(employeeData.results);
-    setDisplayList(employeeData.results);
+    // setDisplayList(employeeData.results);
   }, []);
 
   function handleInputChange(evt) {
@@ -38,6 +38,35 @@ function App() {
       setSortOpt(newSortOpt);
     }
   }
+
+  useEffect(function() {
+    function applyFilter(list) {
+      if (filter.trim() === '') return list;
+      return list.filter(e => e.name.first.toLowerCase().indexOf(filter.toLowerCase())>-1 ||
+                              e.name.last.toLowerCase().indexOf(filter.toLowerCase())>-1);
+    }
+
+    function applySort(list) {
+      if (sortOpt === 'none') {
+        return list;
+      } else {
+        return list.concat().sort(function(a, b) {
+          let aVal = `${a.name.first} ${a.name.last}`;
+          let bVal = `${b.name.first} ${b.name.last}`;
+          if (sortOpt === 'asc') {
+            return (aVal===bVal ? 0 : aVal < bVal ? -1 : 1);
+          } else { // 'desc'
+            return (aVal===bVal ? 0 : aVal < bVal ? 1 : -1)
+          }
+        });
+      }
+    }
+    // console.log(`[useEffect] sort(${sortOpt}) & filter(${filter})`);
+    // re-generate employee list if sort or filter changes
+    let filtered = applyFilter(employeeList);
+    let sortedFiltered = applySort(filtered);
+    setDisplayList(sortedFiltered);
+  }, [sortOpt, filter, employeeList]);
 
   return (
     <>
